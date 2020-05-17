1069f5d98851c1f99de65805029db4b9abda83a0
PrasanthKotturu <kvlprasanth92@gmail.com>
2020-05-17 21:31:52 +0530

    commit

---
+++ DatingApp.API/Data/Datacontext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+        {
+        }
+        public DbSet<Value> Values{get;set;}
+    }
+}
